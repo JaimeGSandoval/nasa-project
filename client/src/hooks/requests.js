71d3431f97c2bf78a3bc5f49d3bc9f911079f79c
JaimeGSandoval <jaime.devs@gmail.com>
2022-09-06 21:02:37 -0700

    Add docker file

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:8000/v1';
+// const API_URL = 'http://localhost:8000/v1'; use for local development
+const API_URL = 'v1'; // use for production
+// we 'll eventually be running our nasa project in the cloud, so we need to update all of our urls that point to localhost to make sure that they work regardless of where we're running our server. Now that our api might be hosted in the cloud or some other domain or ip address that's not localhost, we change the api url to be relative to where the client is hosted, so we just leave the v1 path for version 1 of our api. Now our client will know that it should make requests to the api that's liviing on the same address at the same origin as the frontend. We're serving both the frontend client as well as the server using the same node application. The api and our frontend ae both hosted on the same server, regardless of whether it's localhost or some other domain.
 
 // Load planets and return as JSON
 async function httpGetPlanets() {
